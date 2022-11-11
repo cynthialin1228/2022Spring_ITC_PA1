0c5a93c600a2b90f68563f4b38378b19f36526d4
Cynthia <70316322+cynthialin1228@users.noreply.github.com>
2022-11-11 17:07:38 +0800

    get max h & output max val on bst (top->bottom)

--- bst.cpp
+++ bst.cpp
@@ -8,7 +8,7 @@ using namespace std;
 
 class TreeNode{
     public:
-        int leftPointer, rightPointer, parentPointer, mypointer;
+        int leftPointer, rightPointer, parentPointer, mypointer, nodeHeight;
         int keyValue;
         TreeNode(){}
         ~TreeNode(){}
@@ -18,9 +18,7 @@ void makeTree(int postA[], int length, TreeNode root[],int mypointer, int& h){
     root[mypointer].keyValue=postA[length-1];
     h++;
     cout<<"node="<<root[h].keyValue<<", its address="<<&root[h]<<endl;
-    // cout<<endl<<"root= "<<root.keyValue<<"  ---->  ";
     if(length==1){
-        // cout<<"I'm a leaf"<<endl;
         root[mypointer].leftPointer=-1;
         root[mypointer].rightPointer=-1;
         return;
@@ -34,86 +32,86 @@ void makeTree(int postA[], int length, TreeNode root[],int mypointer, int& h){
         }
         if(k!=length-2){
             int rightTreeNodes[length-k-2], rightlength=length-k-2;
-            // cout<<endl<<"rightT: ";
             for(int i=0; i<rightlength; i++){
                 rightTreeNodes[i]=postA[k+1+i];
-                // cout<<rightTreeNodes[i]<<", ";
             }
-            // copy(postA+k+1,postA+length-2,rightTreeNodes);
             root[mypointer].rightPointer = h;
             root[h].parentPointer = mypointer;
-            // cout<<"rightChildRoot.parentPointer"<<rightChildRoot.parentPointer<<endl;
+			root[h].nodeHeight = root[mypointer].nodeHeight+1;
             makeTree(rightTreeNodes, rightlength, root, h,h);
         }else{
             root[mypointer].rightPointer=-1;
-            // cout<<"no right child"<<endl;
         }
 
         if(k!=-1){
             int leftTreeNodes[k+1], leftlength=k+1;
-            // cout<<endl<<"leftT: ";
             for(int i=0; i<leftlength; i++){
                 leftTreeNodes[i]=postA[i];
-                // cout<<leftTreeNodes[i]<<", ";
             }
-            // copy(postA, postA+k, leftTreeNodes);
             root[mypointer].leftPointer = h;
             root[h].parentPointer = mypointer;
-            // cout<<"leftChildRoot.parentPointer="<<leftChildRoot.parentPointer<<endl;
+			root[h].nodeHeight = root[mypointer].nodeHeight+1;
             makeTree(leftTreeNodes, leftlength, root,h,h);
         }else{
             root[mypointer].leftPointer = -1;
-            // cout<<"no left child"<<endl;
         }
     return;
 }
 void printPreorderTree(TreeNode root[], int mypointer, vector<int>& preT){
     cout<<root[mypointer].keyValue<<", ";
     preT.push_back(root[mypointer].keyValue);
-    if(root[mypointer].leftPointer != -1) printPreorderTree(root,root[mypointer].leftPointer, preT);
-    if(root[mypointer].rightPointer != -1)printPreorderTree(root,root[mypointer].rightPointer, preT);
+    if(root[mypointer].leftPointer != -1){
+    	printPreorderTree(root,root[mypointer].leftPointer, preT);
+	} 
+    if(root[mypointer].rightPointer != -1){
+    	printPreorderTree(root,root[mypointer].rightPointer, preT);
+	} 
     return;
 }    
+int getHeight(TreeNode root[], int length){
+	int maxHeight=0;
+	for(int i=0; i<length; i++){
+		if(root[i].nodeHeight>maxHeight){
+			maxHeight = root[i].nodeHeight;
+		}
+	}
+	return maxHeight;
+}
+void getMaxValues(TreeNode root[],int length, int maxValues[]){
+	for(int i=0; i<length; i++){
+		if(root[i].keyValue>maxValues[root[i].nodeHeight-1]){
+			maxValues[root[i].nodeHeight-1] = root[i].keyValue;
+		}
+	}
+}
+
 int main(int argc, char *argv[]){
     ifstream infile; 
     infile.open(argv[1]);
     ofstream outfile;
     outfile.open(argv[2]);
 
-    // TODO: Please code here
     int treeNum=0;
     infile>>treeNum;
     int **postTree=NULL;
-    // int **preTree=NULL;
-    // int **inorderTree=NULL;
     postTree = new int * [treeNum]; 
-    // preTree = new int * [treeNum]; 
-    // inorderTree = new int * [treeNum];
     TreeNode **binaryTreeRoot;
     binaryTreeRoot = new TreeNode * [treeNum];
-    //TreeNode **bst_tree;
-    //bst_tree = new TreeNode * [treeNum];
 
     int nodeNum[treeNum];
     for(int i=0; i<treeNum; i++){
         infile>>nodeNum[i];
         postTree[i] = new int[nodeNum[i]];
         binaryTreeRoot[i] = new TreeNode[nodeNum[i]];
-        // preTree[i] = new int[nodeNum[i]];
-        // inorderTree[i] = new int[nodeNum[i]];
-        //bst_tree[i] = new TreeNode[nodeNum[i]];
         for(int j=0; j<nodeNum[i]; j++){
             infile>>postTree[i][j];
-            // inorderTree[i][j]=postTree[i][j];
         }
-        // sort(inorderTree[i],inorderTree[i]+nodeNum[i]);
     }
-
 
     for(int i=0; i<treeNum; i++){
         binaryTreeRoot[i][0].parentPointer=-1;
         binaryTreeRoot[i][0].mypointer=0;
-        // cout<<endl<<"makeTree"<<i<<endl;
+		binaryTreeRoot[i][0].nodeHeight=1;
         int h = 0;
         makeTree(postTree[i], nodeNum[i], binaryTreeRoot[i],0, h);
     }
@@ -127,7 +125,18 @@ int main(int argc, char *argv[]){
             outfile<<preArr[j]<<" ";
         }
         outfile<<endl;
+    	int height = getHeight(binaryTreeRoot[i],nodeNum[i]);
+        outfile<<height<<endl;
+        int maxValues[height];
+        for(int j=0; j<height; j++){
+        	maxValues[j]=-1000;
+		}
+        getMaxValues(binaryTreeRoot[i], maxValues);
+        for(int j=0; j<height; j++){
+        	outfile<<maxValues[j]<<" "<<endl;
+		}
     }    
+    
     infile.close();
     outfile.close();
 }
